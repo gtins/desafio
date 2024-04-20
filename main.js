0b3eab31934caad968ddb52ec4a679fc51092e53
Gustavo <gu.martins@catolicasc.edu.br>
2024-04-19 23:45:57 -0300

    minor changes

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 var pesquisarCep = async() => {
     var cep = document.getElementById('inputCEP').value;
-    var url = `http://viacep.com.br/ws/${cep}/json/`;
+    var url = `https://viacep.com.br/ws/${cep}/json/`;
     var dados = await fetch(url);
     var endereco = await dados.json();
     console.log(endereco);
-    //document.getElementById('inputEndereco').value = endereco.cep;
+    document.getElementById('inputEndereco').value = endereco.localidade;
 
 }
