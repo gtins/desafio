ee287112aa4cf9ac7521a345e809c1d0de6bd596
Gustavo <gu.martins@catolicasc.edu.br>
2024-04-21 16:20:04 -0300

    Functions to return data from api

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 var pesquisarCep = async() => {
-    var cep = document.getElementById('inputCEP').value;
+    var cep = document.getElementById('cep').value;
     var url = `https://viacep.com.br/ws/${cep}/json/`;
     var dados = await fetch(url);
     var endereco = await dados.json();
     console.log(endereco);
-    document.getElementById('inputEndereco').value = endereco.localidade;
+    retornaCampos(endereco);    
+}
 
+function retornaCampos(endereco){
+    document.getElementById('endereco').value = endereco.logradouro;
+    document.getElementById('numero').value = endereco.ddd;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = endereco.localidade;
+    document.getElementById('estado').value = endereco.uf;
 }
